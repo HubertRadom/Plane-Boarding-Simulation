9ad9778e7fc38b0f6b4ceb111be3115c687f044a
trebacz626 <trebacz.kacper@gmail.com>
2020-01-12 14:19:56 +0100

    Update main.py
    orders fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def conduct_test(test,visualise,should_stow=True):
   STEPS=[]
   for i in range(ITERATIONS):
     print("Iteration: "+str(i+1)+"/"+str(ITERATIONS))
-    game = Game(test[1],visualise)
+    game = Game(test[1](),visualise)
     STEPS.append(game.play(should_stow))
   plt.hist(STEPS,5,density=True)
   plt.savefig(test[0]+".png")
@@ -16,27 +16,27 @@ def conduct_test(test,visualise,should_stow=True):
 ITERATIONS=100
 VISUALISE=False
 
-tests = [["RANDOM",random_order()],
-["BACK TO FRONT",back_to_front()],
-["FRONT TO BACK",front_to_back()],
-["BACK TO FRONT GROUP 4",back_to_front_4()],
-["FRONT TO BACK GROUP 4",front_to_back_4()],
-["WINDOW MIDDLE ISLE",window_middle_isle()],
-["STEFFEN PERFECT",steffen_perfect()],
-["STEFFEN MODIFIED",steffen_modified()]]
+tests = [["RANDOM",random_order],
+["BACK TO FRONT",back_to_front],
+["FRONT TO BACK",front_to_back],
+["BACK TO FRONT GROUP 4",back_to_front_4],
+["FRONT TO BACK GROUP 4",front_to_back_4],
+["WINDOW MIDDLE ISLE",window_middle_isle],
+["STEFFEN PERFECT",steffen_perfect],
+["STEFFEN MODIFIED",steffen_modified]]
 
 for test in tests:
   conduct_test(test,VISUALISE)
 
 
-no_stowing_tests = [["RANDOM NO STOWING",random_order()],
-["BACK TO FRONT 4 GROUPS NO STOWING",back_to_front_4()]]
+no_stowing_tests = [["RANDOM NO STOWING",random_order],
+["BACK TO FRONT 4 GROUPS NO STOWING",back_to_front_4]]
 
 for test in no_stowing_tests:
   conduct_test(test,VISUALISE,False)
 
-no_shuffling_tests = [["RANDOM NO SHUFFLE",random_without_shuffle()],
-["BACK TO FRONT 4 GROUPS NO SHUFFLE",back_to_front_4_without_shuffle()]]
+no_shuffling_tests = [["RANDOM NO SHUFFLE",random_without_shuffle],
+["BACK TO FRONT 4 GROUPS NO SHUFFLE",back_to_front_4_without_shuffle]]
 
 for test in no_shuffling_tests:
   conduct_test(test,VISUALISE)
